61b7ce4f20b9ea501ff5e599f175847d1ca3a894
Yash Chaudhari <ychaudhari124@gmail.com>
2023-11-22 18:53:23 +0530

    Added CSRF_TRUSTED_ORIGINS

--- django_weather_app/settings.py
+++ django_weather_app/settings.py
@@ -113,8 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-# CSRF_TRUSTED_ORIGINS = ['*']
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = ['https://*.xb0t.pw']
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
