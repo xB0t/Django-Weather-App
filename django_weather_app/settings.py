69bf156772406c8e86ab6a3aa677222d4b7b1a9d
Yash Chaudhari <ychaudhari124@gmail.com>
2023-11-22 18:49:38 +0530

    Added SECURE_PROXY_SSL_HEADER

--- django_weather_app/settings.py
+++ django_weather_app/settings.py
@@ -113,7 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ['*']
+# CSRF_TRUSTED_ORIGINS = ['*']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
