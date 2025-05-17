f074a34f3cc363cfcf583f16ab5b4dd84cfea9a2
Yash Chaudhari <ychaudhari124@gmail.com>
2025-05-17 12:05:11 +0530

    Update settings.py
    #this build for new Docker hub image.

--- django_weather_app/settings.py
+++ django_weather_app/settings.py
@@ -135,6 +135,8 @@ SESSION_COOKIE_SECURE = True
 
 STATIC_URL = 'static/'
 
+#this build for new Docker hub image.
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
